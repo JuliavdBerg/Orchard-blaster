0790941283446c70c692e7b8463156fc548c5624
JuliavdBerg <juliavandenberg11@gmail.com>
2025-09-07 19:55:24 +0200

    kleine fox fix

--- Assets/Scripts/FoxEnemy.cs
+++ Assets/Scripts/FoxEnemy.cs
@@ -5,35 +5,35 @@ public class FoxEnemy : MonoBehaviour
     [SerializeField] private float foxSpeed = 5f;
     private bool movingRight = true;
 
-    void Update()
+    void Update ()
     {
         float moveDirection = movingRight ? 1 : -1;
-        transform.Translate(Vector3.right * moveDirection * foxSpeed * Time.deltaTime);
+        transform.Translate( Vector3.right * moveDirection * foxSpeed * Time.deltaTime );
 
-        if (transform.position.x >= 7.4f && movingRight)
+        if ( transform.position.x >= 7.4f && movingRight ) // turn around
         {
             movingRight = false;
             FlipSprite();
         }
-        else if (transform.position.x <= -7.41f && !movingRight)
+        else if ( transform.position.x <= -7.41f && !movingRight )
         {
             movingRight = true;
             FlipSprite();
         }
     }
-    void FlipSprite()
+    void FlipSprite ()
     {
         Vector3 scale = transform.localScale;
         scale.x *= -1;
         transform.localScale = scale;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D ( Collision2D collision )
     {
-        if (collision.gameObject.CompareTag("apple bullet"))
+        if ( collision.gameObject.CompareTag( "apple bullet" ) )
         {
-            Debug.Log("fox hit");
-            Destroy(gameObject);
+            Debug.Log( "fox hit" );
+            Destroy( gameObject );
         }
     }
 }
